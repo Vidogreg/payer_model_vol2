7d9b6940d4fc7597b5d43826a7d4abc135239eb8
vidogreg <vidogreg@gmail.com>
2019-08-23 15:07:03 +0200

    - added 08 code with result

--- 1_code/08_feature_normalization.R
+++ 1_code/08_feature_normalization.R
@@ -7,7 +7,12 @@
 # ---------------------------------------------------------------------------------------
 # Conclusion
 # ---------------------------------------------------------------------------------------
-# ???
+conclusion <- paste(
+  'Transformation by Box-Cox together with scaling seems to slightly improve',
+  'the performance of the model. We should run CV to be sure, since',
+  'this is only one sample.',
+  sep = '\n'
+)
 
 ## Run utils
 source('1_code/utils/00_run_utils.R')
@@ -20,9 +25,8 @@ config <- list(
   platform = platform,
   dig = 3,
   dataFile = 'payer_model_' %+% project %+% '_GP&iOS_mkt_2019-04-01_2019-06-30.rds',
-  sampleSize = 200000,
+  sampleSize = 800000,
   testSampleSplit = 0.5,
-  # cvRunCount = 1,
   seed = 1
 )
 
@@ -61,18 +65,19 @@ dat$dy_payer <- factor(dat$dy_pay_count > 0)
 dat$player_id <- as.integer(dat$player_id)
 
 ## Define the model
-formulaString <- paste(
-  'dy_payer ~',
-  'dx_pay_count +',
-  'dx_revenue +',
-  'dx_session_count +',
-  'dx_session_time +',
-  'dx_session_days +',
-  'dx_login_count +',
-  'dx_gems_count +',
-  'dx_gems_spent +',
+label <- 'dy_payer'
+features <- c(
+  'dx_pay_count',
+  'dx_revenue',
+  'dx_session_count',
+  'dx_session_time',
+  'dx_session_days',
+  'dx_login_count',
+  'dx_gems_count',
+  'dx_gems_spent',
   'tier'
 )
+formulaString <- paste(label, '~', paste(features, collapse = ' + '))
 
 ## Sample data
 set.seed(config$seed)
@@ -82,43 +87,168 @@ trainRows <- sampleRows[1:iHalf]
 testRows <- sampleRows[(iHalf + 1):config$sampleSize]
 
 ## Train data
-datTrain <- dat[trainRows, ]
+datTrain <- dat[trainRows, ] %>% select(c(features, label))
 
 ## Test data
-datTest <- dat[testRows, ]
+datTest <- dat[testRows, ] %>% select(c(features, label))
 
 
 
-### Train the model
+### Train the models
+## Basic model
 mod <- glm(
   formula = formulaString,
   data = datTrain,
   family = 'binomial'
 )
-datTrain$fit <- predict.glm(mod, newdata = datTrain, type = 'response')
+cutOff <- calculateCutOff(
+  datTrain$dy_payer,
+  predict.glm(mod, newdata = datTrain, type = 'response')
+)
 
-## Calculate RCD-optimal cut-off
-cutOff <- calculateCutOff(datTrain$dy_payer, datTrain$fit)
+## scaled model
+datTrainScale <- predict(preProcess(datTrain, method = c('center', 'scale')), datTrain)
+datTestScale <- predict(preProcess(datTest, method = c('center', 'scale')), datTest)
+modScale <- glm(
+  formula = formulaString,
+  data = datTrainScale,
+  family = 'binomial'
+)
+cutOffScale <- calculateCutOff(
+  datTrainScale$dy_payer,
+  predict.glm(modScale, newdata = datTrainScale, type = 'response')
+)
+
+## scaled and normalized model
+datTrainTrans <- datTrain
+datTestTrans <- datTest
+
+for(f in features) {
+  boxCoxObj <- BoxCoxTrans(datTrain[[f]] + 1)
+  datTrainTrans[[f]] <- predict(boxCoxObj, datTrain[[f]] + 1)
+  datTestTrans[[f]] <- predict(boxCoxObj, datTest[[f]] + 1)
+  # print(boxCoxObj)
+  
+  x <- datTrainTrans[[f]]
+  datTrainTrans[[f]] <- (x - mean(x))/sd(x)
+  x <- datTestTrans[[f]]
+  datTestTrans[[f]] <- (x - mean(x))/sd(x)
+}
+
+modTrans <- glm(
+  formula = formulaString,
+  data = datTrainTrans,
+  family = 'binomial'
+)
+cutOffTrans <- calculateCutOff(
+  datTrainTrans$dy_payer,
+  predict.glm(modTrans, newdata = datTrainTrans, type = 'response')
+)
 
 
 
-### Evaluate the model
-datTest$fit <- predict.glm(mod, newdata = datTest, type = 'response')
-datTest$prediction <- datTest$fit >= cutOff
-modPred <- prediction(datTest$fit, datTest$dy_payer)
-modAuc <- performance(modPred, 'auc')@y.values[[1]]
-modRoc <- performance(modPred, 'tpr', 'fpr')
-modConfMatrix <- table(datTest$prediction, datTest$dy_payer)
+### Evaluate the models
+### And print the results
+evalModel <- function(mod, cutOff, datTest, title = '', note = '') {
+  datTestEval <- datTest
+  datTestEval$fit <- predict.glm(mod, newdata = datTestEval, type = 'response')
+  datTestEval$prediction <- datTestEval$fit >= cutOff
+  modPred <- prediction(datTestEval$fit, datTestEval$dy_payer)
+  modAuc <- performance(modPred, 'auc')@y.values[[1]]
+  modRoc <- performance(modPred, 'tpr', 'fpr')
+  modConfMatrix <- table(datTestEval$prediction, datTestEval$dy_payer)
+  
+  TP <- modConfMatrix[2, 2]
+  TN <- modConfMatrix[1, 1]
+  FP <- modConfMatrix[2, 1]
+  FN <- modConfMatrix[1, 2]
+  
+  ## ROC curve
+  par(pty = 's')
+  print(plot(
+    modRoc,
+    main = paste(title, 'AUC =', round(modAuc, 3))
+  ))
+  par(pty = 'm')
+  
+  ## model performance with RCD-optimal cut-off
+  printOutput(list(
+    note = note,
+    rcd_optimal_confusion_matrix = modConfMatrix,
+    sensitivity = TP/(TP + FN),
+    precision = TP/(TP + FP),
+    relative_count_difference = (FP + TP)/(FN + TP)
+  ))
+}
 
-TP <- modConfMatrix[2, 2]
-TN <- modConfMatrix[1, 1]
-FP <- modConfMatrix[2, 1]
-FN <- modConfMatrix[1, 2]
+filePathOutput <- file.path('2_output', '08_feature_transformation.pdf')
+pdf(filePathOutput)
+printOutput(config)
+printOutput(cat(conclusion))
 
-print(plot(modRoc))
-print(list(
-  rcd_optimal_confusion_matrix = modConfMatrix,
-  sensitivity = TP/(TP + FN),
-  precision = TP/(TP + FP),
-  relative_count_difference = (FP + TP)/(FN + TP)
-))
+evalModel(
+  mod, cutOff, datTest,
+  'basic model', 'no scaling or tranformation'
+)
+evalModel(
+  modScale, cutOffScale, datTestScale,
+  'scaled model',
+  'scaled/standardized features'
+)
+evalModel(
+  modTrans, cutOffTrans, datTestTrans,
+  'transformed model',
+  'features transformed and scaled'
+)
+
+dev.off()
+
+
+
+# ## Downsample train dataset
+# datTrainDown <- data.table(
+#   downSample(x = datTrain %>% select(features), y = datTrain$dy_payer)
+# )
+# setnames(datTrainDown, old = c('Class'), new = c('dy_payer'))
+# 
+# # for(f in features) {
+# for(f in c('x')) {
+#   print(
+#     ggplot(datTrainDown, aes_string(x = f)) +
+#       geom_histogram(bins = 30) +
+#       ggtitle(f)
+#   )
+#   print(
+#     ggplot(datTrainDown, aes_string(x = f)) +
+#       geom_density() +
+#       ggtitle(f)
+#   )
+#   print(
+#     ggplot(datTrainDown, aes_string(sample = f)) +
+#       geom_qq() +
+#       ggtitle(f)
+#   )
+# }
+# 
+# f <- 'dx_session_count'
+# ggplot(datTrainDown, aes_string(sample = f)) +
+#   geom_qq() +
+#   ggtitle(f)
+# shapiro.test(datTrainDown[[f]])
+# 
+# boxCoxObj <- BoxCoxTrans(datTrainDown[[f]] + 1)
+# datTrainDown$x <- predict(boxCoxObj, datTrainDown[[f]] + 1)
+# 
+# ggplot(datTrainDown, aes(sample = x)) +
+#   geom_qq() +
+#   ggtitle(paste(f, 'transformed'))
+# shapiro.test(datTrainDown$x)
+
+
+
+# apply(datTrain %>% select(features), 2, mean)
+# apply(datTrain %>% select(features), 2, sd)
+# apply(datTrainScaled %>% select(features), 2, mean)
+# apply(datTrainScaled %>% select(features), 2, sd)
+# apply(datTrainTrans %>% select(features), 2, mean)
+# apply(datTrainTrans %>% select(features), 2, sd)
